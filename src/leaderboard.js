6904d95f29c6d1c42c7c97d13ede65ea553cd63c
Federico Luna <fluna1973@yahoo.com>
2022-08-22 22:29:32 -0600

    set skeleton structure

---
+++ src/leaderboard.js
@@ -0,0 +1,22 @@
+export default class leaderboard {
+  #leaderboard
+  #gameName 
+  #id
+
+  constructor(gameName, id) {
+    this.#leaderboard = [];
+    this.#gameName = gameName;
+    this.#id = id
+  }
+
+  addScore(name, score){
+    this.#leaderboard.push({
+      name,
+      score,
+    });
+  }
+
+  refreshScoreB(leaderboard){
+    this.#leaderboard = leaderboard
+  }
+}
