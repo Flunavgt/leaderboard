2a6a4ba09a1e60a01b1c047ad7102bbd942f467c
Federico Luna <fluna1973@yahoo.com>
2022-08-23 09:04:55 -0600

    set css wireframe

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 // import _ from 'lodash';
 import './style.css';
 
+const wireframe = [
+  {
+    id:0,
+    name: 'test1',
+    score: '5000',
+  },
+  {
+    id:1,
+    name: 'test1',
+    score: '5000',
+  },
+  {
+    id:2,
+    name: 'test1',
+    score: '5000',
+  },
+  {
+    id:3,
+    name: 'test1',
+    score: '5000',
+  },
+  {
+    id:4,
+    name: 'test1',
+    score: '5000',
+  },
+];
+
+document.getElementById('scoreTable').innerHTML = wireframe.map((items) => `<div class="test1">
+<p class="eachScore">${items.name}: ${items.score}<p>`).join('');
