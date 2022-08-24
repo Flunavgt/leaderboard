c44a1434ec647bb312db6093d6d9960729b5bf75
Federico Luna <fluna1973@yahoo.com>
2022-08-24 07:48:13 -0600

    API connected

--- src/leaderboard.js
+++ src/leaderboard.js
@@ -11,11 +11,30 @@ export default class leaderboard {
     this.#id = id;
   }
 
-  addScore(name, score) {
-    this.#leaderboard.push({
-      name,
-      score,
-    });
+  async addScore(user, score, id, newurl) {
+    const dataToSend = JSON.stringify({ "score": score, "user":`${user}`})
+    const response = await fetch(`${newurl}games/${id}/scores/`, {
+      mode: 'no-cors',
+      credentials: 'include',
+      method: 'POST',
+      body: dataToSend,
+      headers: {
+        'content-type': 'application/json',
+      } 
+    }).catch(err => {
+      console.log(err) });
+
+    return response.json();
+    // this.#leaderboard.push({
+    //   user,
+    //   score,
+    // });
+  }
+
+  getScores(newurl, id) {
+    fetch(`${newurl}/games/${id}/scores/`);
+    const info = response.json();
+    return info.result;
   }
 
   refreshScoreB(leaderboard) {
