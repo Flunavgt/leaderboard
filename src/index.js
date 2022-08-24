2d6fb278f52fd74fc2057ff455934e7f6f601289
Federico Luna <fluna1973@yahoo.com>
2022-08-24 09:44:23 -0600

    corrects indents

--- src/index.js
+++ src/index.js
@@ -1,107 +1,71 @@
 // import _ from 'lodash';
-import { result } from 'lodash';
-import leaderboard from './leaderboard';
+// import { result } from 'lodash';
+// import leaderboard from './leaderboard.js';
 import './style.css';
-
-
-// const wireframe = [
-//   {
-//     id: 0,
-//     name: 'test1',
-//     score: '5000',
-//   },
-//   {
-//     id: 1,
-//     name: 'test2',
-//     score: '5000',
-//   },
-//   {
-//     id: 2,
-//     name: 'test3',
-//     score: '5000',
-//   },
-//   {
-//     id: 3,
-//     name: 'test4',
-//     score: '5000',
-//   },
-//   {
-//     id: 4,
-//     name: 'test5',
-//     score: '5000',
-//   },
-// ];
-
-// document.getElementById('scoreTable').innerHTML = wireframe.map((items) => `
-// <p class="eachScore">${items.name}: ${items.score}</p>`).join(' ');
 
 const newurl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
 const id = 'XJZrqxsxqJ4Bo67bnIka';
 
-let myleaderboard = new leaderboard('tetris', id);
-
-
 const form = document.querySelector('#totalInfo');
 const nameInput = document.querySelector('#name');
-const scoreInput = document.querySelector('#score')
+const scoreInput = document.querySelector('#score');
+
+async function postData(url = '', data = {}) {
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body: data,
+  });
+
+  return response.json();
+}
 
 form.addEventListener('click', () => {
   const user = nameInput.value;
   const score = scoreInput.value;
-  const dataToSend = JSON.stringify({ "score": score, "user":`${user}`})
-  _postData(`${newurl}games/${id}/scores/`, dataToSend )
-  .then(json => {
-      console.log(json) // Handle success
-  })
-  .catch(err => {
-      console.log(err) // Handle errors
-  });
+  const dataToSend = JSON.stringify({ score, user });
+  postData(`${newurl}games/${id}/scores/`, dataToSend)
+    .then((json) => {
+      console.log(json); // Handle success
+    })
+    .catch((err) => {
+      console.log(err); // Handle errors
+    });
   nameInput.value = '';
   scoreInput.value = '';
 });
 
-async function _postData(url = '', data = {}) {
+async function getData(url = '') {
   const response = await fetch(url, {
-      method: 'POST',
-      mode: 'cors',
-      cache: 'no-cache',
-      credentials: 'same-origin',
-      redirect: 'follow',
-      referrerPolicy: 'no-referrer',
-      headers: {
-          "Content-type": "application/json; charset=UTF-8"
-      },
-      body: data
+    method: 'GET',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
   });
-  
-  return response.json();
-}
 
-async function _getData(url = '') {
-  const response = await fetch(url, {
-      method: 'GET',
-      mode: 'cors',
-      cache: 'no-cache',
-      credentials: 'same-origin',
-      redirect: 'follow',
-      referrerPolicy: 'no-referrer',
-  });
-  
   return response.json();
 }
 
 const myScores = () => {
-  const info = _getData(`${newurl}games/${id}/scores/`).then(res =>{
-    document.getElementById('scoreTable').innerHTML = res['result'].map((items) => `
+  getData(`${newurl}games/${id}/scores/`).then((res) => {
+    document.getElementById('scoreTable').innerHTML = res.result.map((items) => `
     <p class="eachScore">${items.user}: ${items.score}</p>`).join(' ');
   });
-}
+};
 
 myScores();
-
 
 const refresh = document.getElementById('refreshBtn');
 
 refresh.addEventListener('click', () => {
   myScores();
-})
+});
--- src/leaderboard.js
+++ src/leaderboard.js
@@ -11,8 +11,9 @@ export default class leaderboard {
     this.#id = id;
   }
 
+  // eslint-disable-next-line class-methods-use-this
   async addScore(user, score, id, newurl) {
-    const dataToSend = JSON.stringify({ "score": score, "user":`${user}`})
+    const dataToSend = JSON.stringify({ score, user });
     const response = await fetch(`${newurl}games/${id}/scores/`, {
       mode: 'no-cors',
       credentials: 'include',
@@ -20,24 +21,9 @@ export default class leaderboard {
       body: dataToSend,
       headers: {
         'content-type': 'application/json',
-      } 
-    }).catch(err => {
-      console.log(err) });
+      },
+    });
 
     return response.json();
-    // this.#leaderboard.push({
-    //   user,
-    //   score,
-    // });
-  }
-
-  getScores(newurl, id) {
-    fetch(`${newurl}/games/${id}/scores/`);
-    const info = response.json();
-    return info.result;
-  }
-
-  refreshScoreB(leaderboard) {
-    this.#leaderboard = leaderboard;
   }
 }
